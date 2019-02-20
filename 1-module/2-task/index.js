54a95bd06b2de1d00f3fa6d199d9e9d73c3582c3
Alexey Khabarov <alexey.khabarov@mail.ru>
2019-02-20 22:59:07 +0300

    solved 1-module/2-task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,6 +12,7 @@ function print (text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid (name) {
+    return ( name !== '' && ~name.indexOf(" ") == 0 && name.length > 3 ) ? true : false;
 }
 
 function sayHello () {
