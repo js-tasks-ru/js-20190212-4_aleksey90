4116d7feb729eeff626872a71d3cc035089deab9
Alexey Khabarov <alexey.khabarov@mail.ru>
2019-02-24 09:18:50 +0300

    solved 2-module/2-task

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,5 +4,26 @@
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
+function find(obj, value) {
+    let result = [];
+    function findPath(obj, path) {
+        for (let key in obj) {
+            let track = (path !== undefined) ? path + '.' + key : key;
+            if (obj[key] === value) result.push(track);
+            if (typeof obj[key] == 'object' && obj[key] != null) {
+                findPath(obj[key], track);
+            }
+        }
+    }
+
+    findPath(obj);
+
+    switch (result.length) {
+        case 0:
+            return null;
+        case 1:
+            return result[0];
+        default:
+            return result;
+    }
 }
