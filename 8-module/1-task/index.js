531f51a0a112a41b79e7b19dd81e2d8001dc9f6b
Alexey Khabarov <alexey.khabarov@mail.ru>
2019-04-09 08:48:10 +0300

    first commit

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -21,12 +21,31 @@ function SortableTable(items) {
      * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
      */
     this.el = document.createElement('table');
+    
+    const tHead = this.el.createTHead();
+    const tBody = this.el.createTBody();
+    
+    const rowHead = tHead.insertRow();
 
+    for (let key in items[0]) {
+        let th = rowHead.insertCell();
+        th.innerText = key[0].toUpperCase() + key.substr(1).toLowerCase();
+    }
+    
+    for (let i = 0; i < items.length; i++) {
+        let rowBody = tBody.insertRow();
+        for (let key in items[i]) {
+            let td = rowBody.insertCell();
+            td.innerText = items[i][key];
+        }
+    }
     /**
      * Метод выполняет сортировку таблицы
      * @param {number} column - номер колонки, по которой нужно выполнить сортировку (отсчет начинается от 0)
      * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
      */
-    this.sort = function (column, desc = false) { };
+    this.sort = function (column, desc = false) { 
+
+    };
 }
 
