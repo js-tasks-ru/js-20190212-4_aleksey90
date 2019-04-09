14b287b29bd1c7ee10c6623a6b77ef346ceaac28
Alexey Khabarov <alexey.khabarov@mail.ru>
2019-04-09 15:49:09 +0300

    Solved 8-module/1-task

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -26,10 +26,9 @@ function SortableTable(items) {
     const tBody = this.el.createTBody();
     
     const rowHead = tHead.insertRow();
-
     for (let key in items[0]) {
         let th = rowHead.insertCell();
-        th.innerText = key[0].toUpperCase() + key.substr(1).toLowerCase();
+        th.innerText = key[0].toUpperCase() + key.substr(1);
     }
     
     for (let i = 0; i < items.length; i++) {
@@ -39,12 +38,29 @@ function SortableTable(items) {
             td.innerText = items[i][key];
         }
     }
+
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+      }
     /**
      * Метод выполняет сортировку таблицы
      * @param {number} column - номер колонки, по которой нужно выполнить сортировку (отсчет начинается от 0)
      * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
      */
     this.sort = function (column, desc = false) { 
+
+        let rows = [...this.el.tBodies[0].rows];
+        
+        if (isNumeric(rows[0].cells[column].innerHTML)) {
+            rows.sort((a, b) => (a.cells[column].innerHTML - b.cells[column].innerHTML));
+        } else {
+            rows.sort((a, b) => (a.cells[column].innerHTML > b.cells[column].innerHTML) ? 1: -1);
+        }
+        if (desc) rows.reverse();
+
+        for (let i = 0; i < rows.length; i++) {
+            this.el.tBodies[0].appendChild(rows[i]);
+        }
 
     };
 }
